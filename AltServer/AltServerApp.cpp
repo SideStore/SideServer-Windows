35a669b0884e26067fe412b9945ce7dc24a3763b
Spidy123222 <64176728+Spidy123222@users.noreply.github.com>
2023-07-30 11:54:44 -0700

    Change sparkle xml locations
    Signed-off-by: Spidy123222 <64176728+Spidy123222@users.noreply.github.com>

--- AltServer/AltServerApp.cpp
+++ AltServer/AltServerApp.cpp
@@ -509,9 +509,9 @@ void AltServerApp::Start(HWND windowHandle, HINSTANCE instanceHandle)
 	_instanceHandle = instanceHandle;
 
 #if STAGING
-	win_sparkle_set_appcast_url("https://altstore.io/altserver/sparkle-windows-staging.xml");
+	win_sparkle_set_appcast_url("https://raw.githubusercontent.com/SideStore/SideServer-Windows/develop/sparkle-windows.xml");
 #else
-	win_sparkle_set_appcast_url("https://altstore.io/altserver/sparkle-windows.xml");
+	win_sparkle_set_appcast_url("https://raw.githubusercontent.com/SideStore/SideServer-Windows/develop/sparkle-windows-staging.xml");
 #endif
 
 	win_sparkle_init();
